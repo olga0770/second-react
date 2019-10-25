eb97e94cd1934ff324229f3e8a36b3b2287f9770
Olga Batrakova <olya0201@gmail.com>
2019-10-25 18:57:35 +0200

    second commit

---
+++ src/components/theme/ThemePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ButtonWithProps from "../button/ButtonWithProps";
+
+export default class ThemePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: ""
+        }
+    }
+
+    submitColorChange = () => {
+        this.props.onColorChange(this.state.color);
+        this.props.history.push("/");
+    }
+
+    render() {
+        console.log(this.props);
+
+        return (
+            <div>
+                <h1>Theme Page</h1>
+                <input type="color" onChange={event => this.setState({color: event.target.value})}></input>
+                <ButtonWithProps informButtonPage={this.submitColorChange}/>
+            </div>
+        );
+    }
+}
+
